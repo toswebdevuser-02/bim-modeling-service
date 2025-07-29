c44eadf3188caaf9cadabb93b41e779c4eca0acd
toswebdevuser-02 <tos.webdevuser02@gmail.com>
2025-07-29 19:05:57 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        <meta name="google-site-verification" content="E9EIdQSB3rcHU7ze9U9YfdXWi0u3V2oayOjHhsYt_Rs" />
       </head>
       <body className="font-body antialiased">{children}</body>
     </html>
